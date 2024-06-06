2d15765530ceae637ebe02f1038b24b23b765433
Steven Hertz <StevenH@appi4.com>
2024-06-06 11:36:51 -0400

    Initial Commit

---
+++ UsableCodeTemplates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UsableCodeTemplates
+//
+//  Created by Steven Hertz on 6/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UsableCodeTemplates/UsableCodeTemplatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsableCodeTemplatesApp.swift
+//  UsableCodeTemplates
+//
+//  Created by Steven Hertz on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct UsableCodeTemplatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
