e7cb10f103d82503632acb19f2e11a65160fb78c
Steven Hertz <StevenH@appi4.com>
2024-06-06 11:46:15 -0400

    Dogs Scrolling

--- UsableCodeTemplates/ContentView.swift
+++ UsableCodeTemplates/ContentView.swift
@@ -7,18 +7,114 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+struct MyContentView {
+    
+    @State var currentIndex: Int = 0
+        
+}
+
+extension MyContentView {
+     
+}
+
+extension MyContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:16) {
+//            firstViewFromFunction()
+            secondViewFromFunction()
+//            thirdViewFromFunction()
+//            fourthViewFromFunction()
+//            Text("Current Image: \(images[currentIndex])")
+//                .font(.headline)
+//                .padding()
         }
         .padding()
     }
 }
 
+
+extension MyContentView {
+    func firstViewFromFunction() -> some View {
+        let images = ["d1", "d2", "d3", "d4","d1", "d2", "d3", "d4" ]
+        return ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(images, id: \.self) { item in
+                    Image(item)
+                        .resizable()
+                        .cornerRadius(25)
+                        .frame(width: 200, height:200 )
+                        .shadow(radius: 10, y: 10)
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .frame(height: 260)
+        .safeAreaPadding(32)
+        .scrollClipDisabled()
+        .scrollTargetBehavior(.viewAligned)
+            //        .contentMargins(20)
+            //        .background {Color(.systemGray4)}    }
+    }
+    func secondViewFromFunction() -> some View {
+        let images = ["d1", "d2", "d3", "d4", ]
+        return ScrollView(.horizontal) {
+            LazyHStack(content: {
+                ForEach(images, id: \.self) { item in
+                    Image(item)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .containerRelativeFrame(.horizontal)
+                        .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
+                        .clipped()
+                    
+                        .scrollTransition { content, phase in
+                            content
+                                .scaleEffect(x: phase.isIdentity ? 1.0 : 0.8)
+                                .scaleEffect(y: phase.isIdentity ? 1.0 : 0.8)
+                                .offset(y: phase.isIdentity ? 0 : 140)
+                        }
+                }
+            })
+        }
+        .contentMargins(20)
+        .background {Color(.systemGray4)}   
+    }
+    
+    func thirdViewFromFunction() -> some View {
+        Text("From Third View")
+    }
+
+    func fourthViewFromFunction() -> some View {
+//        let images = ["d1", "d2", "d3", "d4","d1", "d2", "d3", "d4" ]
+//        return ScrollView(.horizontal, showsIndicators: false) { 
+//            HStack {
+//                ForEach(images.enumerated(), id: \.self) { index, item in
+//                    Image(item)
+//                        .resizable()
+//                        .cornerRadius(25)
+//                        .frame(width: 200, height:200 )
+//                        .shadow(radius: 10, y: 10)
+//                        .onAppear {
+//                            currentIndex = index
+//                        }
+//                }
+//            }
+//            .scrollTargetLayout()
+//        }
+//        .frame(height: 260)
+//        .safeAreaPadding(32)
+//        .scrollClipDisabled()
+//        .scrollTargetBehavior(.viewAligned)
+//        //        .contentMargins(20)
+//        //        .background {Color(.systemGray4)}    }
+        EmptyView()
+    }
+
+    
+}
+
+
 #Preview {
-    ContentView()
+    MyContentView()
 }
